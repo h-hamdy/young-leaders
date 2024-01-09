f0109fa7a460351cb03769f445e5daf18ea4885d
h-hamdy <houssamhamdy223@gmail.com>
2024-01-09 12:26:27 +0100

    SignIn From validation

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import * as z from "zod";
 import {
   Button,
   Checkbox,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
@@ -14,6 +16,9 @@ import {
 import { Link } from "react-router-dom";
 import Loader from "../../components/shared/Loader";
 import EnterEmail from "../../components/Modal/EnterEmail";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignInValidation } from "../../lib/validation";
 
 const SigninForm = () => {
   const [isLoading, SetisLoading] = React.useState(false);
@@ -21,6 +26,21 @@ const SigninForm = () => {
   const handleClick = () => setShow(!show);
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const form = useForm<z.infer<typeof SignInValidation>>({
+    resolver: zodResolver(SignInValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignInValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
     <div className="max-w-[400px] flex flex-col items-center justify-center gap-[15px]">
@@ -36,36 +56,57 @@ const SigninForm = () => {
           Sign In to join the Yong leaders Comminuty
         </p>
       </Flex>
-      <FormControl isRequired>
-        <FormLabel fontSize="sm">Email</FormLabel>
-        <Input type="email" name="title" />
-      </FormControl>
-      <FormControl isRequired>
-        <FormLabel fontSize="sm">Password</FormLabel>
-
-        <InputGroup size="md" className="focus:outline-none focus:border-black">
-          <Input pr="4.5rem" type={show ? "text" : "password"} />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="xs" onClick={handleClick}>
-              {show ? "Hide" : "Show"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <Button
-        onClick={() => SetisLoading(!isLoading)}
-        colorScheme="primary"
-        className="bg-primary w-full transition-colors duration-100 hover:bg-primary-hover"
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-col gap-[25px]"
       >
-        {isLoading ? (
-          <div className="flex gap-2">
-            <Loader size="sm" />
-            <div>Loading...</div>
-          </div>
-        ) : (
-          <div>Sign In</div>
-        )}
-      </Button>
+        <FormControl isRequired isInvalid={form.formState.errors.email}>
+          <FormLabel fontSize="sm">Email</FormLabel>
+          <Input type="text" {...form.register("email")} />
+          <FormErrorMessage>
+            {form.formState.errors.email?.message}
+          </FormErrorMessage>
+        </FormControl>
+
+        <FormControl isRequired isInvalid={form.formState.errors.password}>
+          <FormLabel fontSize="sm">Password</FormLabel>
+
+          <InputGroup
+            size="md"
+            className="focus:outline-none focus:border-black"
+          >
+            <Input
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              {...form.register("password")}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="xs" onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            {form.formState.errors.password?.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          type="submit"
+          onClick={() => SetisLoading(!isLoading)}
+          isLoading={form.formState.isSubmitting}
+          colorScheme="primary"
+          className="bg-primary w-full transition-colors duration-100 hover:bg-primary-hover"
+        >
+          {isLoading ? (
+            <div className="flex gap-2">
+              <Loader size="sm" />
+              <div>Loading...</div>
+            </div>
+          ) : (
+            <div>Sign In</div>
+          )}
+        </Button>
+      </form>
       <Flex className="justify-between w-full">
         <Checkbox size="sm" className="text-xs">
           Remember me
@@ -78,8 +119,7 @@ const SigninForm = () => {
         </button>
       </Flex>
 
-      
-	  <EnterEmail isOpen={isOpen} onClose={onClose}/>
+      <EnterEmail isOpen={isOpen} onClose={onClose} />
       <button className="text-sm">
         Don't have an account?{" "}
         <Link to="/sign-up">
